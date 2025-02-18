481d34b891fbaef5b06b11c3016f4c8c727c31a2
taiwithers <59430904+taiwithers@users.noreply.github.com>
2025-02-18 15:21:59 -0500

    basic mvp

---
+++ mvp/pdb_test.py
@@ -0,0 +1,13 @@
+from ipdb import set_trace as breakpoint # overriding builtin breakpoint()
+
+print('Initial print statement')
+
+for i in range(10):
+  print(f'{i=}')
+  if i == 5:
+    breakpoint()
+
+
+raise Exception()
+
+
